b331b8a6e8c74a2c70b92e76a44167a6cb18a084
AriSheva <ari.wmi@gmail.com>
2018-02-03 22:06:46 +0700

    Ubah

---
+++ Artikel.php
@@ -0,0 +1,3 @@
+<html>
+	<h1>Ini artikel</h1>
+<html>
--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 		<title>i-Dev Git | Home</title>
 	</head>
 	<body>
-		<h1>Hello Git</h1>
+		<h1>Hello Ari indra</h1>
 		<table border="1">
 			<tr>
 				<th><a href="home.php">Home</a></th>
 				<th><a href="about.php">About</a></th>
 			</tr>
 			<tr>
-				<td colspan="2">Ini Home</td>
+				<td colspan="2">Newbie</td>
 			</tr>
 			<tr>
 				<td colspan="2">Footer I-Dev &copy; 2018</td>
